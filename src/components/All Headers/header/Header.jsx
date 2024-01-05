42f452269e5cfb6225a5d4080b3777a24bb83fe8
Ripty-Dx <riptyfullstack.dx@gmail.com>
2024-01-05 15:08:03 +0530

    Report name fix 5-1-24

--- src/components/All Headers/header/Header.jsx
+++ src/components/All Headers/header/Header.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import "./index.css";
 const Header = () => {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+  const path = window.location.pathname;
   return (
     <div id={`${styles.main}`} className="d-flex justify-content-between  align-items-center my-2 gap-1">
       <p className={`m-0 ${styles.text}`}>
@@ -38,21 +39,44 @@ const Header = () => {
       <p className={`m-0  ${styles.text}`}>
         <Link to="" className="linkStyle">
           <div class="dropdown dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Reports
-            {/* </a> */}
+            {path === "/sales-report" ? "Sales Report" : null || path === "/newness-report" ? "newness Report" : null || path === "/comparison-report" ? "comparison Report" : null || "Reports"}
             <ul class="dropdown-menu">
               <li>
-                <Link to="/sales-report" className="dropdown-item text-start" onClick={()=>navigate("/sales-report")}>
+                <Link
+                  to="/sales-report"
+                  className="dropdown-item text-start"
+                  onClick={() => {
+                    // setReportName("Sales report");
+                    navigate("/sales-report");
+                    // console.log(reportName);
+                  }}
+                >
                   Sales Report
                 </Link>
               </li>
               <li>
-                <Link to="/newness-report" className="dropdown-item text-start" onClick={()=>navigate("/newness-report")}>
+                <Link
+                  to="/newness-report"
+                  className="dropdown-item text-start"
+                  onClick={() => {
+                    // setReportName("newness report");
+                    navigate("/newness-report");
+                    // console.log(reportName);
+                  }}
+                >
                   Newness Report
                 </Link>
               </li>
               <li>
-                <Link to="/comparison-report" className="dropdown-item  text-start"  onClick={()=>navigate("/comparison-report")}>
+                <Link
+                  to="/comparison-report"
+                  className="dropdown-item  text-start"
+                  onClick={() => {
+                    // setReportName("Comparison report");
+                    navigate("/comparison-report");
+                    // console.log(reportName);
+                  }}
+                >
                   Comparison Report
                 </Link>
               </li>
