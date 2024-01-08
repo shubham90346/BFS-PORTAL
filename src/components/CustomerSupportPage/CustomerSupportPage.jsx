b2cab2c63ae9932a8ab6b3777aa01ef38f8d9c8e
Ripty-Dx <riptyfullstack.dx@gmail.com>
2024-01-08 16:12:34 +0530

    filters in customer support

--- src/components/CustomerSupportPage/CustomerSupportPage.jsx
+++ src/components/CustomerSupportPage/CustomerSupportPage.jsx
@@ -2,111 +2,53 @@ import React, { useMemo } from 'react'
 import Styles from './Style.module.css'
 import MySupportTicket from './MySupportTicket';
 import { Link } from 'react-router-dom'
-import { CustomerServiceIcon, OrderStatusIcon,MarketingSupportIcon,DefaultSupportIcon } from '../../lib/svg'
+import { CustomerServiceIcon, OrderStatusIcon } from '../../lib/svg'
 
-function CustomerSupportPage({ data, PageSize, currentPage }) {
-    return (
-        <div>
-            <div className=''>
-                <div className={Styles.supportMain}>
-                    <div className='row'>
-                        <div className='col-lg-3 col-md-12 col-sm-12'>
-                            <div className={Styles.supportLeft}>
+function CustomerSupportPage({ data, PageSize, currentPage, manufacturerFilter, searchBy }) {
+  console.log(data, manufacturerFilter, searchBy);
+  const filteredData = useMemo(() => {
+    let newValues = data;
+    if (manufacturerFilter) {
+      newValues = newValues.filter((item) => item.ManufacturerId__c === manufacturerFilter);
+    }
+    if (searchBy) {
+      newValues = newValues?.filter((value) => value.CaseNumber?.toLowerCase().includes(searchBy?.toLowerCase()));
+    }
+    console.log("new values", newValues);
+    return newValues;
+  }, [data, manufacturerFilter, searchBy]);
+  return (
+    <div>
+      <div className="">
+        <div className={Styles.supportMain}>
+          <div className="row">
+            <div className="col-lg-3 col-md-12 col-sm-12">
+              <div className={Styles.supportLeft}>
+                <Link to={"/order-list"}>
+                  <div className={Styles.supportLeftBox}>
+                    <div className={Styles.supportLeftImg}>
+                      <OrderStatusIcon width={42} height={42} />
+                    </div>
 
-                                <Link to={'/order-list'}>
-                                    <div className={Styles.supportLeftBox}>
-                                        <div className={Styles.supportLeftImg}>
-                                            <OrderStatusIcon width={42} height={42} />
-                                        </div>
+                    <div className={Styles.supportLeftContent}>
+                      <h2>Order Status</h2>
+                      <p>Track Your Orders with Ease.</p>
+                    </div>
+                  </div>
+                </Link>
 
-                                        <div className={Styles.supportLeftContent}>
-                                            <h2>Order Status</h2>
-                                            <p>Track Your Orders with Ease.</p>
+                <div className={Styles.supportLeftBox}>
+                  <div className={Styles.supportLeftImg}>
+                    <CustomerServiceIcon width={42} height={42} />
+                  </div>
 
-                                        </div>
-                                    </div>
-                                </Link>
-
-                                <Link to={'/order-list'}>
-                                <div className={Styles.supportLeftBox}>
-                                    <div className={Styles.supportLeftImg}>
-                                        <CustomerServiceIcon width={42} height={42} />
-                                    </div>
-
-                                    <div className={Styles.supportLeftContent}>
-                                        <h2>Customer Services </h2>
-                                        <p>Resolving Concerns Serving Solutions</p>
-
-                                    </div>
-
-                                </div>
-                                </Link>
-
-                                <Link to={'/order-list'}>
-                                <div className={Styles.supportLeftBox}>
-                                    <div className={Styles.supportLeftImg}>
-                                        <MarketingSupportIcon width={42} height={42} />
-                                    </div>
-
-                                    <div className={Styles.supportLeftContent}>
-                                        <h2>Marketing Support Issues </h2>
-                                        <p>Elevate Your Marketing with Proactive Solutions.</p>
-
-                                    </div>
-
-                                </div>
-                                </Link>
-
-                                <Link to={'/order-list'}>
-                                <div className={Styles.supportLeftBox}>
-                                    <div className={Styles.supportLeftImg}>
-                                        <DefaultSupportIcon width={42} height={42} />
-                                    </div>
-
-                                    <div className={Styles.supportLeftContent}>
-                                        <h2>Management Cases </h2>
-                                        <p>Empowering Solutions for Effective Management</p>
-
-                                    </div>
-
-                                </div>
-                                </Link>
-
-                                <Link to={'/order-list'}>
-                                <div className={Styles.supportLeftBox}>
-                                    <div className={Styles.supportLeftImg}>
-                                        <DefaultSupportIcon width={42} height={42} />
-                                    </div>
-
-                                    <div className={Styles.supportLeftContent}>
-                                        <h2>DIF Tester Issue </h2>
-                                        <p>Empowering Solutions for Effective Management</p>
-
-                                    </div>
-
-                                </div>
-                                </Link>
-
-                                <Link to={'/order-list'}>
-                                <div className={Styles.supportLeftBox}>
-                                    <div className={Styles.supportLeftImg}>
-                                        <DefaultSupportIcon width={42} height={42} />
-                                    </div>
-
-                                    <div className={Styles.supportLeftContent}>
-                                        <h2>Displays Issues </h2>
-                                        <p>Empowering Solutions for Effective Management</p>
-
-                                    </div>
-
-                                </div>
-                                </Link>
-
-                            </div>
-
-
-                        </div>
-
+                  <div className={Styles.supportLeftContent}>
+                    <h2>Customer Services </h2>
+                    <p>Resolving Concerns Serving Solutions</p>
+                  </div>
+                </div>
+              </div>
+            </div>
 
             <div className="col-lg-9 col-md-12 col-sm-12">
               {filteredData.length ? (
@@ -114,17 +56,12 @@ function CustomerSupportPage({ data, PageSize, currentPage }) {
               ) : (
                 <div className="flex justify-center items-center py-4 w-full lg:min-h-[300px] xl:min-h-[380px]">No data found</div>
               )}
-                        </div>
-
-                    </div>
-
-                </div>
-
             </div>
-
-
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default CustomerSupportPage
+export default CustomerSupportPage;
