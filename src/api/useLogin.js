7f639acba26690dda32adb5a2727a15500aea52d
Ripty-Dx <riptyfullstack.dx@gmail.com>
2024-01-09 12:25:44 +0530

    Login Validation added

--- src/api/useLogin.js
+++ src/api/useLogin.js
@@ -4,7 +4,6 @@ const useLogin = () => {
   return {
     mutateLogin: async (email, password) => {
       const response = await axios.post("https://dev.beautyfashionsales.com/beauty/v3/PYmsWL", { email, password });
-      // console.log(response.data);
       localStorage.setItem("response",JSON.stringify(response))
       return response.data;
     },
