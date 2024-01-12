d6a4811b1d09da8a855ea0d4f821056d9d3b177f
Ripty-Dx <riptyfullstack.dx@gmail.com>
2024-01-12 16:22:11 +0530

    Login UI responsiveness added

--- src/components/All Headers/loginHeader/LoginHeader.jsx
+++ src/components/All Headers/loginHeader/LoginHeader.jsx
@@ -12,34 +12,29 @@ const LoginHeader = () => {
             <div className="d-flex justify-content-between align-items-center gap-2">
               <div className="d-flex justify-content-center align-items-center gap-5">
                 {/* <Link to="/dashboard" className=""> */}
-                  <img src={"/assets/images/BFSG_logo.svg"} alt="img" />
+                <img src={"/assets/images/BFSG_logo.svg"} alt="img" />
                 {/* </Link> */}
               </div>
-              <div className="d-flex justify-content-center align-items-center gap-4">
-              <p className={`m-0 ${style.language} ${style.text} flex`}>
-                          <div className="dropdown d-flex justify-content-center align-items-center " role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Need Help?&nbsp; <NeedHelp/>
-            {/* </a> */}
-            <ul className="dropdown-menu">
-              <li>
-                <Link to="" className="dropdown-item text-start" >
-                Order Status
-                </Link>
-              </li>
-              <li>
-                <Link to="t" className="dropdown-item text-start" >
-                Management Cases
-                </Link>
-              </li>
-              <li>
-                <Link to="" className="dropdown-item  text-start"  >
-                Marketing Support Issues
-                </Link>
-              </li>
-            </ul>
-          </div>
-              {/* <img src={"/assets/images/dropDownArrow.svg"} alt="img" /> */}
-            </p>
+              <div className="d-flex justify-content-center align-items-center flex-wrap gap-4">
+                <p className={`m-0 ${style.language} ${style.text} flex`}>
+                  <div className="dropdown d-flex justify-content-center align-items-center " role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Need Help?&nbsp; <NeedHelp />
+                    {/* </a> */}
+                    <ul className="dropdown-menu">
+                      <li>
+                        <Link to="" className="dropdown-item text-start">
+                          Order Status
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="t" className="dropdown-item text-start">
+                          Customer Services
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  {/* <img src={"/assets/images/dropDownArrow.svg"} alt="img" /> */}
+                </p>
                 <p className={`m-0 ${style.language}`}>Sign up</p>
               </div>
             </div>
